2d6e2d35d1b1d5d0b1c444e9004abe6b748af4ba
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-06-02 00:59:38 -0600

    settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        images: {
-            allowFutureImage: true
-        },
-    },
 };
 
 export default nextConfig;
