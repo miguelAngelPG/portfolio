a5b4d8d63f0c6f823087ed074e6d42851c5d67ae
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-06-02 01:00:27 -0600

    add languages

---
+++ src/languages/en.ts
@@ -0,0 +1,21 @@
+import { ILanguage } from "@/types";
+
+export const en: ILanguage = {
+    slogan: "Im a passionate web development and design UI/UX.",
+    experiences: [
+        {
+            startDate: 2023,
+            endDate: 2023,
+            jobPosition: 'Developer Frontend Senior',
+            company: 'Empresa A',
+            description: ['Lideré el desarrollo de una nueva interfaz de usuario.', 'Implementé nuevas funcionalidades con React y Tailwind CSS.'],
+        },
+        {
+            startDate: 2023,
+            endDate: 2023,
+            jobPosition: 'Desarrollador Frontend',
+            company: 'Empresa B',
+            description: ['Trabajé en el mantenimiento y mejora de aplicaciones web existentes.', 'Colaboré en el diseño de componentes reutilizables.'],
+        },
+    ],
+}
---
+++ src/languages/es.ts
@@ -0,0 +1,21 @@
+import { ILanguage } from "@/types";
+
+export const es: ILanguage = {
+    slogan: "Soy un apasionado del desarrollo web y diseño UI/UX.",
+    experiences: [
+        {
+            startDate: 2023,
+            endDate: 2023,
+            jobPosition: 'Desarrollador Frontend Senior',
+            company: 'Empresa A',
+            description: ['Lideré el desarrollo de una nueva interfaz de usuario.', 'Implementé nuevas funcionalidades con React y Tailwind CSS.'],
+        },
+        {
+            startDate: 2023,
+            endDate: 2023,
+            jobPosition: 'Desarrollador Frontend',
+            company: 'Empresa B',
+            description: ['Trabajé en el mantenimiento y mejora de aplicaciones web existentes.', 'Colaboré en el diseño de componentes reutilizables.'],
+        },
+    ],
+}
