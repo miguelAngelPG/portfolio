68d827bafc80b07dec9132ea9b8c97920e6feff8
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-06-05 14:28:22 -0600

    add title section

---
+++ src/components/TitleSection.tsx
@@ -0,0 +1,8 @@
+
+export const TitleSection = ({title}: {title: string}) => {
+    return (
+        <h2 className='text-3xl font-bold text-gray-900 dark:text-white mb-8'>
+            {title}
+        </h2>
+    )
+}
