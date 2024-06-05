b48461fe4348ebf4e7ae720ea8783e38d44d0231
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-06-05 14:28:38 -0600

    new info.

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -1,65 +1,74 @@
 import { ILanguage } from "@/types";
 
 export const en: ILanguage = {
-    slogan: "Im a passionate web development and design UI/UX.",
-    experiences: [
-        {
-            startDate: new Date('10-10-2022'),
-            endDate: new Date('01-01-2024'),
-            jobPosition: 'Software Analyst Programmer',
-            company: 'Erycia',
-            link: 'https://www.erycia.com/',
-            description: [
-                `Databases: Managed and optimized SQL Server and Informix databases.`,
-                'Web Development: Created scalable applications with .NET, applying SOLID principles.',
-                'User Interfaces: Designed dynamic interfaces using JavaScript, jQuery, Bootstrap, CSS3, and HTML5.',
-                'Requirements Analysis: Defined functionalities and objectives with users and stakeholders.',
-                'Applications and Servers: Developed web and desktop applications, and servers with C#.',
-                'File Manipulation: Generated custom PDF and XLS files.',
-                'Testing and Debugging: Conducted testing and debugging to ensure quality.',
-                'System Integration: Established secure connections between databases and systems.',
-                'Technical Support: Provided technical support and maintenance for applications and networks.',
-            ]
-        },
-        {
-            startDate: new Date('01-01-2022'),
-            endDate: new Date('09-09-2022'),
-            jobPosition: 'Full-Stack Web Developer',
-            company: 'Karem Dayan',
-            link: 'https://www.karemdayan.com/',
-            description: [
-                'Web Development: Created scalable, high-performance web applications with React JS and Next JS, using ECMAScript 6 and TypeScript.',
-                'User Interfaces: Designed attractive, dynamic interfaces with CSS3 and custom animations.',
-                'Reusable Components: Built functional, reusable components, enhancing efficiency and maintainability.',
-                'State Management: Used Redux for application state management, ensuring a smooth user experience.',
-                'Authentication: Implemented authentication and access control, ensuring application security.',
-                'Databases: Worked with Firebase and MongoDB, optimizing performance for various data models.',
-                'Prototyping: Collaborated on designing prototypes, wireframes, and mockups to define application architecture.',
-                'Responsive Design: Implemented responsive designs and used Material UI for adaptable interfaces.',
-                'Robust APIs: Developed APIs with Node.js and JSON Web Tokens for secure authentication and authorization.',
-                'Version Control: Utilized Git and GitHub for source code management and collaboration.',
-                'Email Functionality: Implemented email sending with customized event invitations to Google Calendar.',
-                'Content Management: Developed a system for secure and efficient video and image management.',
-            ]
-        },
-        {
-            startDate: new Date('06-06-2019'),
-            endDate: new Date('09-09-2021'),
-            jobPosition: 'JR Web developer',
-            company: 'Vlim',
-            link: 'https://www.vlim.com.mx/',
-            description: [
-                'Agile Methodologies: Participated in web development projects using Agile (SCRUM), achieving efficient deliveries and meeting deadlines.',
-                'HTML5 Structuring: Structured and designed pages with HTML5.',
-                'User Interfaces: Designed and developed intuitive, responsive interfaces with HTML5, CSS3, JavaScript, jQuery, and Bootstrap.',
-                'Client-Server Logic: Implemented client-side and server-side logic for dynamic, functional web applications.',
-                'API Integration: Consumed and managed data from web services and APIs, ensuring seamless frontend integration.',
-                'Database Management: Designed and managed relational databases (MySQL), optimizing performance and ensuring data integrity.',
-                'Clean Code: Developed clean, scalable, maintainable code following industry best practices.',
-                'Advanced Features: Implemented advanced features like dynamic email sending, PDF, TXT, XLSX generation, and table pagination.',
-                'React JS Development: Contributed to web applications using React JS, enhancing user experience.',
-                'Version Control: Utilized Git and GitHub for source code management and collaboration.',
-            ],
-        },
-    ],
+    tooltipTheme: "Change theme",
+    hero: {
+        introduce: "Im a passionate web development and design UI/UX.",
+        tooltipLanguages: "Change language",
+        tooltipResume: "The resume'll be downloaded in the current lang. ",
+    },
+    experience: {
+        titleExperience: "Experience",
+        experiences: [
+            {
+                startDate: new Date('10-10-2022'),
+                endDate: new Date('01-01-2024'),
+                jobPosition: 'Software Analyst Programmer',
+                company: 'Erycia',
+                link: 'https://www.erycia.com/',
+                description: [
+                    `Databases: Managed and optimized SQL Server and Informix databases.`,
+                    'Web Development: Created scalable applications with .NET, applying SOLID principles.',
+                    'User Interfaces: Designed dynamic interfaces using JavaScript, jQuery, Bootstrap, CSS3, and HTML5.',
+                    'Requirements Analysis: Defined functionalities and objectives with users and stakeholders.',
+                    'Applications and Servers: Developed web and desktop applications, and servers with C#.',
+                    'File Manipulation: Generated custom PDF and XLS files.',
+                    'Testing and Debugging: Conducted testing and debugging to ensure quality.',
+                    'System Integration: Established secure connections between databases and systems.',
+                    'Technical Support: Provided technical support and maintenance for applications and networks.',
+                ]
+            },
+            {
+                startDate: new Date('01-01-2022'),
+                endDate: new Date('09-09-2022'),
+                jobPosition: 'Full-Stack Web Developer',
+                company: 'Karem Dayan',
+                link: 'https://www.karemdayan.com/',
+                description: [
+                    'Web Development: Created scalable, high-performance web applications with React JS and Next JS, using ECMAScript 6 and TypeScript.',
+                    'User Interfaces: Designed attractive, dynamic interfaces with CSS3 and custom animations.',
+                    'Reusable Components: Built functional, reusable components, enhancing efficiency and maintainability.',
+                    'State Management: Used Redux for application state management, ensuring a smooth user experience.',
+                    'Authentication: Implemented authentication and access control, ensuring application security.',
+                    'Databases: Worked with Firebase and MongoDB, optimizing performance for various data models.',
+                    'Prototyping: Collaborated on designing prototypes, wireframes, and mockups to define application architecture.',
+                    'Responsive Design: Implemented responsive designs and used Material UI for adaptable interfaces.',
+                    'Robust APIs: Developed APIs with Node.js and JSON Web Tokens for secure authentication and authorization.',
+                    'Version Control: Utilized Git and GitHub for source code management and collaboration.',
+                    'Email Functionality: Implemented email sending with customized event invitations to Google Calendar.',
+                    'Content Management: Developed a system for secure and efficient video and image management.',
+                ]
+            },
+            {
+                startDate: new Date('06-06-2019'),
+                endDate: new Date('09-09-2021'),
+                jobPosition: 'JR Web developer',
+                company: 'Vlim',
+                link: 'https://www.vlim.com.mx/',
+                description: [
+                    'Agile Methodologies: Participated in web development projects using Agile (SCRUM), achieving efficient deliveries and meeting deadlines.',
+                    'HTML5 Structuring: Structured and designed pages with HTML5.',
+                    'User Interfaces: Designed and developed intuitive, responsive interfaces with HTML5, CSS3, JavaScript, jQuery, and Bootstrap.',
+                    'Client-Server Logic: Implemented client-side and server-side logic for dynamic, functional web applications.',
+                    'API Integration: Consumed and managed data from web services and APIs, ensuring seamless frontend integration.',
+                    'Database Management: Designed and managed relational databases (MySQL), optimizing performance and ensuring data integrity.',
+                    'Clean Code: Developed clean, scalable, maintainable code following industry best practices.',
+                    'Advanced Features: Implemented advanced features like dynamic email sending, PDF, TXT, XLSX generation, and table pagination.',
+                    'React JS Development: Contributed to web applications using React JS, enhancing user experience.',
+                    'Version Control: Utilized Git and GitHub for source code management and collaboration.',
+                ],
+            },
+        ],
+        button: "Show more"
+    }
 }
--- src/languages/es.ts
+++ src/languages/es.ts
@@ -1,65 +1,74 @@
 import { ILanguage } from "@/types";
 
 export const es: ILanguage = {
-    slogan: "Soy un apasionado del desarrollo web y diseño UI/UX.",
-    experiences: [
-        {
-            startDate: new Date('10-10-2022'),
-            endDate: new Date('01-01-2024'),
-            jobPosition: 'Analista programador de software',
-            company: 'Erycia',
-            link: 'https://www.erycia.com/',
-            description: [
-                'Bases de Datos: Gestioné y optimicé bases de datos SQL Server e Informix.',
-                'Desarrollo Web: Creé aplicaciones escalables con .NET, aplicando principios SOLID.',
-                'Interfaces de Usuario: Diseñé interfaces dinámicas con JavaScript, jQuery, Bootstrap, CSS3 y HTML5.',
-                'Análisis de Requisitos: Definí funcionalidades y objetivos con usuarios y stakeholders.',
-                'Aplicaciones y Servidores: Desarrollé aplicaciones web y de escritorio, y servidores con C#.',
-                'Manipulación de Archivos: Generé archivos PDF y XLS personalizados.',
-                'Pruebas y Correcciones: Realicé pruebas y correcciones para asegurar calidad.',
-                'Integración de Sistemas: Establecí conexiones seguras entre bases de datos y sistemas.',
-                'Soporte Técnico: Ofrecí soporte técnico y mantenimiento de aplicaciones y redes.',
-            ]
-        },
-        {
-            startDate: new Date('01-01-2022'),
-            endDate: new Date('09-09-2022'),
-            jobPosition: 'Desarrollador web Full-Stack',
-            company: 'Karem Dayan',
-            link: 'https://www.karemdayan.com/',
-            description: [
-                'Desarrollo Web: Creé aplicaciones web escalables y de alto rendimiento con React JS y Next JS, usando ECMAScript 6 y TypeScript.',
-                'Interfaces de Usuario: Diseñé interfaces atractivas y dinámicas con CSS3 y animaciones personalizadas.',
-                'Componentes Reutilizables: Construí componentes funcionales y reutilizables, mejorando la eficiencia y mantenibilidad del código.',
-                'Gestión de Estado: Utilicé Redux para gestionar el estado de las aplicaciones, asegurando una experiencia fluida.',
-                'Autenticación: Implementé autenticación y control de acceso, garantizando la seguridad de la aplicación.',
-                'Bases de Datos: Trabajé con Firebase y MongoDB, optimizando el rendimiento para distintos modelos de datos.',
-                'Prototipos: Colaboré en el diseño de prototipos, wireframes y mockups para definir la arquitectura de las aplicaciones.',
-                'Diseño Responsive: Implementé diseños responsive y utilicé Material UI para interfaces adaptables.',
-                'APIs Robustas: Desarrollé APIs con Node.js y tokens web JSON para asegurar autenticación y autorización.',
-                'Control de Versiones: Usé Git y GitHub para gestionar el código fuente y colaborar con otros desarrolladores.',
-                'Funcionalidad de Correo: Implementé envío de correos con invitaciones personalizadas a Google Calendar.',
-                'Gestión de Contenidos: Desarrollé un sistema para cargar, almacenar y visualizar videos e imágenes de manera segura y eficiente.',
-            ]
-        },
-        {
-            startDate: new Date('06-06-2019'),
-            endDate: new Date('09-09-2021'),
-            jobPosition: 'Desarrollador web JR.',
-            company: 'Vlim',
-            link: 'https://www.vlim.com.mx/',
-            description: [
-                'Metodologías Ágiles: Participé en proyectos de desarrollo web utilizando metodologías ágiles (SCRUM), logrando entregas eficientes y cumpliendo plazos.',
-                'Estructuración HTML5: Estructuré y diseñé páginas con HTML5.',
-                'Interfaces de Usuario: Diseñé y desarrollé interfaces intuitivas y responsivas con HTML5, CSS3, JavaScript, jQuery y Bootstrap.',
-                'Lógica Cliente-Servidor: Implementé lógica del lado del cliente y del servidor para aplicaciones web dinámicas y funcionales.',
-                'Integración de APIs: Consumí y gestioné datos de servicios web y APIs, asegurando una perfecta integración con el frontend.',
-                'Gestión de Bases de Datos: Diseñé y gestioné bases de datos relacionales (MySQL), optimizando rendimiento y asegurando la integridad de los datos.',
-                'Código Limpio: Desarrollé código limpio, escalable y mantenible, siguiendo las mejores prácticas de la industria.',
-                'Funcionalidades Avanzadas: Implementé funcionalidades avanzadas como envío de correos dinámicos, generación de archivos PDF, TXT, XLSX y paginación de tablas.',
-                'Desarrollo con React JS: Contribuí al desarrollo de aplicaciones web con React JS, mejorando la experiencia del usuario.',
-                'Control de Versiones: Utilicé Git y GitHub para gestionar el código fuente y colaborar con otros desarrolladores.',
-            ],
-        },
-    ],
+    tooltipTheme: "Cambiar tema",
+    hero: {
+        introduce: "Soy un apasionado del desarrollo web y diseño UI/UX.",
+        tooltipLanguages: "Cambiar idioma",
+        tooltipResume: "El currículum se descargará en el idioma actual.",
+    },
+    experience: {
+        titleExperience: "Experiencia",
+        experiences: [
+            {
+                startDate: new Date('10-10-2022'),
+                endDate: new Date('01-01-2024'),
+                jobPosition: 'Analista programador de software',
+                company: 'Erycia',
+                link: 'https://www.erycia.com/',
+                description: [
+                    'Bases de Datos: Gestioné y optimicé bases de datos SQL Server e Informix.',
+                    'Desarrollo Web: Creé aplicaciones escalables con .NET, aplicando principios SOLID.',
+                    'Interfaces de Usuario: Diseñé interfaces dinámicas con JavaScript, jQuery, Bootstrap, CSS3 y HTML5.',
+                    'Análisis de Requisitos: Definí funcionalidades y objetivos con usuarios y stakeholders.',
+                    'Aplicaciones y Servidores: Desarrollé aplicaciones web y de escritorio, y servidores con C#.',
+                    'Manipulación de Archivos: Generé archivos PDF y XLS personalizados.',
+                    'Pruebas y Correcciones: Realicé pruebas y correcciones para asegurar calidad.',
+                    'Integración de Sistemas: Establecí conexiones seguras entre bases de datos y sistemas.',
+                    'Soporte Técnico: Ofrecí soporte técnico y mantenimiento de aplicaciones y redes.',
+                ]
+            },
+            {
+                startDate: new Date('01-01-2022'),
+                endDate: new Date('09-09-2022'),
+                jobPosition: 'Desarrollador web Full-Stack',
+                company: 'Karem Dayan',
+                link: 'https://www.karemdayan.com/',
+                description: [
+                    'Desarrollo Web: Creé aplicaciones web escalables y de alto rendimiento con React JS y Next JS, usando ECMAScript 6 y TypeScript.',
+                    'Interfaces de Usuario: Diseñé interfaces atractivas y dinámicas con CSS3 y animaciones personalizadas.',
+                    'Componentes Reutilizables: Construí componentes funcionales y reutilizables, mejorando la eficiencia y mantenibilidad del código.',
+                    'Gestión de Estado: Utilicé Redux para gestionar el estado de las aplicaciones, asegurando una experiencia fluida.',
+                    'Autenticación: Implementé autenticación y control de acceso, garantizando la seguridad de la aplicación.',
+                    'Bases de Datos: Trabajé con Firebase y MongoDB, optimizando el rendimiento para distintos modelos de datos.',
+                    'Prototipos: Colaboré en el diseño de prototipos, wireframes y mockups para definir la arquitectura de las aplicaciones.',
+                    'Diseño Responsive: Implementé diseños responsive y utilicé Material UI para interfaces adaptables.',
+                    'APIs Robustas: Desarrollé APIs con Node.js y tokens web JSON para asegurar autenticación y autorización.',
+                    'Control de Versiones: Usé Git y GitHub para gestionar el código fuente y colaborar con otros desarrolladores.',
+                    'Funcionalidad de Correo: Implementé envío de correos con invitaciones personalizadas a Google Calendar.',
+                    'Gestión de Contenidos: Desarrollé un sistema para cargar, almacenar y visualizar videos e imágenes de manera segura y eficiente.',
+                ]
+            },
+            {
+                startDate: new Date('06-06-2019'),
+                endDate: new Date('09-09-2021'),
+                jobPosition: 'Desarrollador web JR.',
+                company: 'Vlim',
+                link: 'https://www.vlim.com.mx/',
+                description: [
+                    'Metodologías Ágiles: Participé en proyectos de desarrollo web utilizando metodologías ágiles (SCRUM), logrando entregas eficientes y cumpliendo plazos.',
+                    'Estructuración HTML5: Estructuré y diseñé páginas con HTML5.',
+                    'Interfaces de Usuario: Diseñé y desarrollé interfaces intuitivas y responsivas con HTML5, CSS3, JavaScript, jQuery y Bootstrap.',
+                    'Lógica Cliente-Servidor: Implementé lógica del lado del cliente y del servidor para aplicaciones web dinámicas y funcionales.',
+                    'Integración de APIs: Consumí y gestioné datos de servicios web y APIs, asegurando una perfecta integración con el frontend.',
+                    'Gestión de Bases de Datos: Diseñé y gestioné bases de datos relacionales (MySQL), optimizando rendimiento y asegurando la integridad de los datos.',
+                    'Código Limpio: Desarrollé código limpio, escalable y mantenible, siguiendo las mejores prácticas de la industria.',
+                    'Funcionalidades Avanzadas: Implementé funcionalidades avanzadas como envío de correos dinámicos, generación de archivos PDF, TXT, XLSX y paginación de tablas.',
+                    'Desarrollo con React JS: Contribuí al desarrollo de aplicaciones web con React JS, mejorando la experiencia del usuario.',
+                    'Control de Versiones: Utilicé Git y GitHub para gestionar el código fuente y colaborar con otros desarrolladores.',
+                ],
+            },
+        ],
+        button: "Ver más"
+    }
 }
