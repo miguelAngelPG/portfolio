2a24aa340cb122485b424323ec81d8b96c664f22
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-06-06 08:55:50 -0600

    text align left

--- src/components/TitleSection.tsx
+++ src/components/TitleSection.tsx
@@ -1,7 +1,7 @@
 
 export const TitleSection = ({title}: {title: string}) => {
     return (
-        <h2 className='text-3xl font-bold text-gray-900 dark:text-white mb-8'>
+        <h2 className='text-3xl font-bold text-gray-900 dark:text-white mb-8 w-full text-start'>
             {title}
         </h2>
     )
