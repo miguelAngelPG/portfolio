aed47b8eac63cbe8ed42b62114af635f48e99329
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-06-08 01:13:15 -0600

    ser up interface to projects and tools

--- src/types/index.ts
+++ src/types/index.ts
@@ -20,3 +20,19 @@ export interface IExperience {
     link: string;
     description: string[];
 }
+
+export interface ITools {
+    tool: Tool;
+    icon: string;
+    invert?: boolean;
+}
+
+export type Tool = 'React JS' | 'Next.js' | 'TailwindCSS' | 'TypeScript' | 'Node.js' | 'Git' | 'GitHub' | 'HTML5' | 'CSS3' | 'JavaScript' | 'Bootstrap';
+
+export interface IProject {
+    title: string;
+    description: string;
+    link: string;
+    image: string;
+    tools: Tool[];
+}
