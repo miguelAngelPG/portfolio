9a3b8821350172bf6b438f29f7f948db881be941
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-06-13 00:21:40 -0600

    change size of icons tools

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -4,7 +4,12 @@ import { tools } from '@/types/constans.type'
 import Image from 'next/image'
 import { Tool } from '@/types'
 
-export const Tools = ({ usedTools }: { usedTools: Tool[]}) => {
+const sizes = {
+    small: 20,
+    normal: 28,
+    large: 32,
+};
+export const Tools = ({ usedTools, size, direction = 'top' }: { usedTools: Tool[], size: 'small' | 'normal' | 'large', direction?: 'top' | 'bottom'}) => {
 
     const toolsFiltered = tools.filter(({tool}) => usedTools.includes(tool))
 
@@ -13,8 +18,8 @@ export const Tools = ({ usedTools }: { usedTools: Tool[]}) => {
             {
                 toolsFiltered.map(({tool, icon, invert}, index) => (
                     <div className={`relative group/tooltip`} key={tool}>
-                        <Tooltip text={tool} direction='top'/>
-                        <Image src={icon} className={`${!!invert && 'dark:invert'}`} alt={tool} width={30} height={30}/>
+                        <Tooltip text={tool} direction={direction}/>
+                        <Image src={icon} className={`${!!invert && 'dark:invert'}`} alt={tool} width={sizes[size]} height={sizes[size]}/>
                     </div>
                 ))
             }
