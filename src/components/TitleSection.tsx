b41f7bc984d5b0751faef7ff148631545776b748
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-06-14 00:50:22 -0600

    add icon to title component

--- src/components/TitleSection.tsx
+++ src/components/TitleSection.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image"
 
-export const TitleSection = ({title}: {title: string}) => {
+export const TitleSection = ({title, icon}: {title: string, icon?: string}) => {
     return (
-        <h2 className='text-3xl font-bold text-gray-900 dark:text-white mb-8 w-full text-start'>
+        <h2 className='text-3xl font-bold text-gray-900 dark:text-white mb-8 w-full text-start flex gap-3 '>
             {title}
+            {
+                icon && <Image src={`/assets/imgs/icons/${icon}.svg`} className='dark:invert h-10 w-auto' alt={icon} width={5} height={5}/>
+            }
         </h2>
     )
 }
