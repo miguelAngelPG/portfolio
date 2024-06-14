9cea6c11a766a0c4b4c8ccb537f9a8ffc69634aa
Miguel Pacheco <angelpachecogranillo@gmail.com>
2024-06-14 10:52:56 -0600

    change slogan

--- src/components/Introduce.tsx
+++ src/components/Introduce.tsx
@@ -21,7 +21,7 @@ export const Introduce = () => {
         <div className='flex flex-col items-left gap-2'>
             <h1 ref={element} className='text-2xl sm:text-4xl text-center sm:text-left font-AileronBold text-slate-900 dark:text-slate-100'>Miguel Angel</h1>
             <h1 className='text-2xl sm:text-4xl text-center sm:text-left font-AileronBold text-slate-900 dark:text-slate-100'>Pacheco Granillo</h1>
-            <p className='text-slate-700 text-sm sm:text-lg dark:text-slate-300 max-w-xl rounded-2xl font-AileronRegular tracking-[0.04rem]'>{ lang.slogan }</p>
+            <p className='text-slate-700 text-sm sm:text-lg dark:text-slate-300 max-w-xl rounded-2xl font-AileronRegular tracking-[0.04rem]'>{ lang.hero.introduce }</p>
         </div>
     )
 }
